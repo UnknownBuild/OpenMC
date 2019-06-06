bfe85d86ac6d77d938c27d80f886db007f78097c
CookiesChen <1109349604@qq.com>
2019-06-06 12:26:24 +0800

    add setting

---
+++ OpenMC/SceneSettings.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+#include "Scene.h"
+#include "../Camera/Camera.h"
+#include "../SpriteRenderer/SpriteRenderer.h"
+
+class SceneSettings final : public Scene {
+public:
+    virtual void Start() override;
+    virtual void Update() override;
+    virtual void Terminate() override;
+
+private:
+    SpriteRenderer* renderer;
+    Camera* camera;
+
+    // test
+    glm::vec3 testColor = glm::vec3(0.5, 0.5, 0.5);
+};
